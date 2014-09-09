2363c03a5bd20706e950dc41495377c8f3b193a0
Brian Lalor <blalor@bravo5.org>
2014-09-09 08:00:48 -0400

    Add support for grub2

--- utils/create-ami.sh
+++ utils/create-ami.sh
@@ -8,8 +8,12 @@ function die() {
     exit 1
 }
 
+function usage() {
+    die "usage: $0 <image> <ami name> <ebs block device> <ebs vol id> <virtualization-type> [<grub-ver>]"
+}
+
 [ $EUID -eq 0 ] || die "must be root"
-[ $# -eq 5 ] || die "usage: $0 <image> <ami name> <ebs block device> <ebs vol id> <virtualization-type>"
+[ $# -eq 5 ] || [ $# -eq 6 ] || usage
 
 _basedir="$( cd $( dirname -- $0 )/.. && /bin/pwd )"
 
@@ -19,30 +23,34 @@ block_dev="${3}"
 img_target_dev="${block_dev}1"
 vol_id="${4}"
 virt_type="${5}"
+shift 5
 
 [ "${virt_type}" = "hvm" ] || [ "${virt_type}" = "paravirtual" ] || die "virtualization type must be hvm or paravirtual"
 
-if [ "${virt_type}" == "paravirtual" ]; then
-    kernel_id="--kernel-id aki-919dcaf8" ## specific to us-east-1!
+if [ "${virt_type}" = "hvm" ] && [ $# -ne 1 ]; then
+    usage
+fi
+
+if [ "${virt_type}" = "paravirtual" ]; then
+    grub_ver="not applicable"
+
+    ## http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/UserProvidedKernels.html
+    ## pv-grub-hd0_1.04-x86_64.gz; specific to us-east-1!
+    kernel_id="--kernel-id aki-919dcaf8"
 
     ## http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/block-device-mapping-concepts.html
     root_device="/dev/sda"
-elif [ "${virt_type}" == "hvm" ]; then
+elif [ "${virt_type}" = "hvm" ]; then
+    grub_ver="${1}"
+    
+    [ "${grub_ver}" = "grub0" ] || [ "${grub_ver}" = "grub2" ] || die "grub version must be grub0 or grub2"
+
     ## kernel id only applies for paravirtualized instances
     kernel_id=""
     
     ## the root device for the block device mapping
     root_device="/dev/xvda"
 fi
-
-echo "executing with..."
-echo "img: ${img}"
-echo "ami_name: ${ami_name}"
-echo "block_dev: ${block_dev}"
-echo "vol_id: ${vol_id}"
-echo "virt_type: ${virt_type}"
-echo "kernel_id: ${kernel_id}"
-echo "root_device: ${root_device}"
 
 ## A client error (InvalidAMIName.Malformed) occurred when calling the
 ## RegisterImage operation: AMI names must be between 3 and 128 characters long,
@@ -57,13 +65,22 @@ fi
 #     exit 1
 # fi
 
+echo "executing with..."
+echo "    img: ${img}"
+echo "    ami_name: ${ami_name}"
+echo "    block_dev: ${block_dev}"
+echo "    vol_id: ${vol_id}"
+echo "    virt_type: ${virt_type}"
+echo "    grub_ver: ${grub_ver}"
+echo "    kernel_id: ${kernel_id}"
+echo "    root_device: ${root_device}"
+
 ## check for required programs
-which aws >/dev/null 2>&1 || die "need aws"
-which curl >/dev/null 2>&1 || die "need curl"
-which jq >/dev/null 2>&1 || die "need jq"
-which e2fsck >/dev/null 2>&1 || die "need e2fsck"
+which aws       >/dev/null 2>&1 || die "need aws"
+which curl      >/dev/null 2>&1 || die "need curl"
+which jq        >/dev/null 2>&1 || die "need jq"
+which e2fsck    >/dev/null 2>&1 || die "need e2fsck"
 which resize2fs >/dev/null 2>&1 || die "need resize2fs"
-which patch >/dev/null 2>&1 || die "need patch"
 
 ## the block device must exist
 [ -e "${block_dev}" ] || die "${block_dev} does not exist"
@@ -74,7 +91,7 @@ my_instance_id="$( curl -s http://169.254.169.254/latest/meta-data/instance-id )
 ## set up/verify aws credentials and settings
 ## http://docs.aws.amazon.com/cli/latest/userguide/cli-chap-getting-started.html
 export AWS_DEFAULT_REGION="$( curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone | sed -e 's#.$##g' )"
-[ -n "${AWS_ACCESS_KEY_ID}" ] || die "AWS_ACCESS_KEY_ID not set"
+[ -n "${AWS_ACCESS_KEY_ID}" ]     || die "AWS_ACCESS_KEY_ID not set"
 [ -n "${AWS_SECRET_ACCESS_KEY}" ] || die "AWS_SECRET_ACCESS_KEY not set"
 
 if [ "$( aws ec2 describe-images --filters "Name=name,Values=${ami_name}" | jq -r '.Images | length' )" -ne 0 ]; then
@@ -97,7 +114,7 @@ hdparm -z ${block_dev}
 
 ## partition volume
 ## http://telinit0.blogspot.com/2011/12/scripting-parted.html
-## need to leave more space for grub; start partition on 2nd cylinder
+## need to leave more space for grub2; start partition on 2nd cylinder
 ## http://serverfault.com/questions/523985/fixing-a-failed-grub-upgrade-on-raid
 parted ${block_dev} --script -- mklabel msdos
 parted ${block_dev} --script -- mkpart primary ext2 1 -1 ## validated by running grub2-install
@@ -121,35 +138,42 @@ fi
 e2fsck -f ${img_target_dev}
 resize2fs ${img_target_dev}
 
-if [ $# -eq 5 ]; then
-    if [ "${5}" == "hvm" ]; then
-        ## special hvm ami stuff, all about fixing up the bootloader
+if [ "${virt_type}" = "hvm" ]; then
+    ## install grub bootloader for hvm instances; they boot like real hardware
 
-        # patch grub-install then install grub on the volume
-        # https://bugs.archlinux.org/task/30241 for where and why for the patch
-        ## https://raw.githubusercontent.com/mozilla/build-cloud-tools/master/ami_configs/centos-6-x86_64-hvm-base/grub-install.diff
-        if [ ! -e /sbin/grub-install.orig ]; then
-            cp /sbin/grub-install /sbin/grub-install.orig
+    # mount the volume so
+    vol_mnt="/mnt/ebs_vol"
+    mkdir -p ${vol_mnt}
+    mount -t ext4 ${img_target_dev} ${vol_mnt}
+    
+    ## mount special filesystems so the chroot is like running in a real
+    ## instance.  hopefully this is pretty distro- and kernel-agnostic…
+    mount -t proc none ${vol_mnt}/proc
+    mount -o bind /dev ${vol_mnt}/dev
+    mount -o bind /sys ${vol_mnt}/sys
+    
+    if [ ${grub_ver} = "grub0" ]; then
+        # make ${vol_mnt}/boot/grub/device.map with contents
+        #   (hd0) ${block_dev}
+        # because otherwise grub-install isn't happy, even with --recheck
+        echo "(hd0) ${block_dev}" > ${vol_mnt}/boot/grub/device.map
+        
+        ## also need to create fake /etc/mtab so grub-install can figure out
+        ## what device / is
+        echo "${img_target_dev} / ext4 rw,relatime 0 0" > ${vol_mnt}/etc/mtab
+        
+        chroot ${vol_mnt} /sbin/grub-install --no-floppy ${block_dev}
 
-            ## only patch once
-            patch --no-backup-if-mismatch -N -p0 -i ${_basedir}/utils/grub-install.diff /sbin/grub-install
-        fi
-
-        # mount the volume so we can install grub and fix the /boot/grub/device.map file (otherwise grub can't find the device even with --recheck)
-        vol_mnt="/mnt/ebs_vol"
-        mkdir -p ${vol_mnt}
-        mount -t ext4 ${img_target_dev} ${vol_mnt}
-
-        # make ${vol_mnt}/boot/grub/device.map with contents "(hd0) ${block_dev}" because otherwise grub-install isn't happy, even with --recheck
-        echo "(hd0)    ${block_dev}" > ${vol_mnt}/boot/grub/device.map
-
-        grub-install --root-directory=${vol_mnt} --no-floppy ${block_dev}
-
-        # ok grub is installed, now redo device.map for booting the actual volume... because otherwise this bloody well doesn't work
-        sed -i -r "s/^(\(hd0\)\s+\/dev\/)[a-z]+$/\1xvda/" ${vol_mnt}/boot/grub/device.map
-
-        umount ${vol_mnt}
+        rm -f ${vol_mnt}/etc/mtab ${vol_mnt}/boot/grub/device.map
+    elif [ ${grub_ver} = "grub2" ]; then
+        ## *aaah* so much simpler!
+        
+        ## @todo move grub2-mkconfig to ami-creator (add grub2 support)
+        chroot ${vol_mnt} /sbin/grub2-mkconfig -o /boot/grub2/grub.cfg
+        chroot ${vol_mnt} /sbin/grub2-install ${block_dev}
     fi
+
+    umount ${vol_mnt}/{proc,dev,sys,}
 fi
 
 ## create a snapshot of the volume
