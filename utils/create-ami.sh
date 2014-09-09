e9e00bb68b0ae286f0f47a06c6cffb2f73d679f2
Brian Lalor <blalor@bravo5.org>
2014-09-08 21:44:52 -0400

    Do not generate image, use pre-generated one
    Plus other cleanup

--- utils/create-ami.sh
+++ utils/create-ami.sh
@@ -9,37 +9,34 @@ function die() {
 }
 
 [ $EUID -eq 0 ] || die "must be root"
+[ $# -eq 5 ] || die "usage: $0 <image> <ami name> <ebs block device> <ebs vol id> <virtualization-type>"
 
 _basedir="$( cd $( dirname -- $0 )/.. && /bin/pwd )"
 
-[ $# -eq 4 -o $# -eq 5 ] || die "usage: $0 <kickstart config file> <ami name> <ebs block device> <ebs vol id> [<virtualization-type>]"
-
-config="$( readlink -f ${1} )"
+img="$( readlink -f ${1} )"
 ami_name="${2}"
 block_dev="${3}"
 img_target_dev="${block_dev}1"
-
 vol_id="${4}"
-virt_type=""
-kernel_id="--kernel-id aki-919dcaf8" ## specific to us-east-1!
+virt_type="${5}"
 
-## http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/block-device-mapping-concepts.html
-root_device="/dev/sda"
+[ "${virt_type}" = "hvm" ] || [ "${virt_type}" = "paravirtual" ] || die "virtualization type must be hvm or paravirtual"
 
-if [ $# -eq 5 ]; then
-    virt_type="--virtualization-type ${5}"
+if [ "${virt_type}" == "paravirtual" ]; then
+    kernel_id="--kernel-id aki-919dcaf8" ## specific to us-east-1!
 
-    if [ "${5}" == "hvm" ]; then
-        ## kernel id only applies for paravirtualized instances
-        kernel_id=""
-        
-        ## the root device for the block device mapping
-        root_device="/dev/xvda"
-    fi
+    ## http://docs.aws.amazon.com/AWSEC2/latest/UserGuide/block-device-mapping-concepts.html
+    root_device="/dev/sda"
+elif [ "${virt_type}" == "hvm" ]; then
+    ## kernel id only applies for paravirtualized instances
+    kernel_id=""
+    
+    ## the root device for the block device mapping
+    root_device="/dev/xvda"
 fi
 
 echo "executing with..."
-echo "config: ${config}"
+echo "img: ${img}"
 echo "ami_name: ${ami_name}"
 echo "block_dev: ${block_dev}"
 echo "vol_id: ${vol_id}"
@@ -60,13 +57,6 @@ fi
 #     exit 1
 # fi
 
-## change to a well-known directory; doesn't have to make sense, just has to be
-## consistent.
-cd "$( dirname ${config} )"
-
-name="$( basename $config | sed -r -e 's#\.[^.]+$##g' )"
-dest_img="${name}.img"
-
 ## check for required programs
 which aws >/dev/null 2>&1 || die "need aws"
 which curl >/dev/null 2>&1 || die "need curl"
@@ -74,7 +64,6 @@ which jq >/dev/null 2>&1 || die "need jq"
 which e2fsck >/dev/null 2>&1 || die "need e2fsck"
 which resize2fs >/dev/null 2>&1 || die "need resize2fs"
 which patch >/dev/null 2>&1 || die "need patch"
-rpm -q python-imgcreate >/dev/null 2>&1 || die "need python-imgcreate package"
 
 ## the block device must exist
 [ -e "${block_dev}" ] || die "${block_dev} does not exist"
@@ -94,13 +83,6 @@ fi
 
 if [ "$( aws ec2 describe-volumes --volume-ids ${vol_id} | jq -r .Volumes[].Attachments[].InstanceId )" != "${my_instance_id}" ]; then
     die "volume ${vol_id} is not attached to this instance!"
-fi
-
-## create the image
-if [ ! -e "${dest_img}" ]; then
-    ${_basedir}/ami_creator/ami_creator.py -c "${config}" -n "${name}"
-else
-    echo "$dest_img already exists; not recreating"
 fi
 
 ## ok, this is fucked up.  the dd writing the image to the volume is exiting
@@ -126,12 +108,12 @@ hdparm -z ${block_dev}
 
 ## write image to volume
 echo "writing image to ${img_target_dev}"
-dd if=${dest_img} of=${img_target_dev} conv=fsync oflag=sync bs=8k
+dd if=${img} of=${img_target_dev} conv=fsync oflag=sync bs=8k
 
 ## force-check the filesystem; re-write the image if it fails
 if ! fsck.ext4 -n -f ${img_target_dev} ; then
     echo "well, that didn't work; trying again"
-    dd if=${dest_img} of=${img_target_dev} conv=fsync oflag=sync bs=8k
+    dd if=${img} of=${img_target_dev} conv=fsync oflag=sync bs=8k
     fsck.ext4 -n -f ${img_target_dev}
 fi
 
@@ -171,7 +153,7 @@ if [ $# -eq 5 ]; then
 fi
 
 ## create a snapshot of the volume
-snap_id=$( aws ec2 create-snapshot --volume-id ${vol_id} --description "root image for ${name}" | jq -r .SnapshotId )
+snap_id=$( aws ec2 create-snapshot --volume-id ${vol_id} --description "root image for ${ami_name}" | jq -r .SnapshotId )
 
 while [ $( aws ec2 describe-snapshots --snapshot-ids ${snap_id} | jq -r .Snapshots[].State ) != "completed" ]; do
     echo "waiting for snapshot ${snap_id} to complete"
@@ -188,15 +170,16 @@ image_id=$( \
     --name "${ami_name}" \
     --root-device-name ${root_device} \
     --block-device-mappings "[{\"DeviceName\":\"${root_device}\",\"Ebs\":{\"SnapshotId\":\"${snap_id}\",\"VolumeSize\":10}},{\"DeviceName\":\"/dev/sdb\",\"VirtualName\":\"ephemeral0\"}]" \
-    ${virt_type} \
+    --virtualization-type ${virt_type} \
     | jq -r .ImageId
 )
 
 echo "created AMI with id ${image_id}"
 
+## create json file next to input image
 {
     echo "{"
     echo "    \"snapshot_id\": \"${snap_id}\", "
     echo "    \"ami_id\": \"${image_id}\""
     echo "}"
-} > "${config}.json"
+} > "${img%.*}.json"
